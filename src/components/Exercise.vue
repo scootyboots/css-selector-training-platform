e58d790038c275b758a1d57f97caea3b42db3d00
scootyboots <scott.coffrin@gmail.com>
2022-03-09 20:25:57 +0900

    rever to 10e85012172f094470a917707c50e9f8fde60b1d

--- src/components/Exercise.vue
+++ src/components/Exercise.vue
@@ -29,6 +29,8 @@ const props = defineProps<ExerciseProps>()
     <div class="two-pain-grid">
     <div class="two-pain-grid__left">
       <div class="two-pain-grid__left-container">
+        <!-- TODO: refactor to move Progress into the app component -->
+        <!-- <Progress /> -->
         <Prompt 
           header="Example Exercise" 
           :explanationPs="explanationArray"
--- src/router/index.ts
+++ src/router/index.ts
@@ -7,9 +7,7 @@ import ExampleAll from '../exercises/ExampleAll.vue'
 import ExampleTesting from '../exercises/ExampleTesting.vue'
 
 export const routes = [
-  { path: paths.home, redirect: () => paths.exampleSingle },
-  { path: '/example-exercise', redirect: () => paths.exampleSingle },
-  // { path: paths.home, component: ExampleSingle },
+  { path: paths.home, component: HelloWorld },
   { path: paths.exampleSingle, component:  ExampleSingle },
   { path: paths.exampleAll, component: ExampleAll },
   { path: paths.exampleTesting, component: ExampleTesting }
