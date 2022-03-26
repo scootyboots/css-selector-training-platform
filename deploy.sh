4562551447e202a1ed0a39e01c2bc1fac285795b
scootyboots <scott.coffrin@gmail.com>
2022-03-26 17:50:24 +0900

    change shell script

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ git checkout -b main
 git add -A
 git commit -m 'deploy'
 
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
